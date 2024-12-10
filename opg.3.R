1d148e5e08fc16dbea7bd69f091c852f753881a4
TobiasJVF <tobiasfrantsen@hotmail.com>
2024-12-10 09:18:27 +0100

    Add files via upload
    Rettet

--- opg.3.R
+++ opg.3.R
@@ -6,9 +6,9 @@ library(ordinal)
 library(tidyverse)
 library(RMariaDB)
 
-# normalt read.csv virker ikke godt med store csv-file, derfor anvend her package data.table.
-# funktion "fread" er samme som read.csv men hurtigere, alle control i read.csv som nrows, colclasses, sep blive håndtere automatisk
-# encoding "Latin-1" kan håndtere dansk bogstaver æ, ø, å
+# normalt virker read.csv ikke godt med store csv-filer, derfor anvendes her package data.table.
+# funktion "fread" er den samme som read.csv men hurtigere, al kontrol i read.csv som nrows, colclasses og sep blive håndtere automatisk
+# encoding "Latin-1" håndterer danske bogstaver æ, ø, å
 
 df <- fread("regnskaber_industri_transport_byg_5_25000_ansatte_anonym.csv", encoding = "Latin-1")
 
@@ -24,7 +24,7 @@ df <- merge(df, branchkode[, c("BRANCHEKODE", "GRP10HOVEDAFDELING")],
             by = "BRANCHEKODE", 
             all.x = TRUE)
 
-# lave graf med branchen
+# lave graf med brancher
 
 df_b <- df %>%
   mutate(`Hvordan ser du mulighederne for at låne penge til din virksomhed? (fiktivt spørgsmål)` = case_when(
@@ -66,12 +66,12 @@ df_branchen$Branche = factor(df_branchen$Branche,
                                         "Info/kom",            
                                         "Service",              
                                         "Ejendom"             
-                                       ))       
+                             ))       
 
 ggplot(df_branchen, aes(x = `Branche`, y = nettotal)) +
   geom_bar(stat = "identity", aes(fill = `Branche`)) +
   geom_text(aes(label = round(nettotal, 2)), vjust = -0.5) +  
-  labs(title = "Ejendomshandel og udlejnings branchen synes selv\nde har størest muglihed for lån",
+  labs(title = "Ejendomshandel og udlejnings branchen synes selv\nde har størst mulighed for lån",
        x = "Antal branchen",
        y = "Nettotal",
        fill = "Antal branchen") +  
@@ -79,7 +79,7 @@ ggplot(df_branchen, aes(x = `Branche`, y = nettotal)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),plot.title = element_text(size = 12))
 
 
-# for lav en grafisk illustration af fordelingen af svarene på spørgsmålet skal vi have en frekvense table
+# for at lave en grafisk illustration af fordelingen af svarene på spørgsmålet skal vi have en frekvens-tabel
 # der findes både dårlige og dårlig under kolonen
 df$`Hvordan ser du mulighederne for at låne penge til din virksomhed? (fiktivt spørgsmål)` <- gsub("Dårlige", "Dårlig", df$`Hvordan ser du mulighederne for at låne penge til din virksomhed? (fiktivt spørgsmål)`)
 
@@ -87,25 +87,25 @@ svar_fordeling <- table(df[,1])
 svar_df <- data.frame(Svar = names(svar_fordeling), Antal = as.numeric(svar_fordeling))
 
 svar_df$Svar = factor(svar_df$Svar,
-                             levels = c("Ved ikke",      
-                                        "Meget dårlige",              
-                                        "Dårlig",            
-                                        "Neutrale",            
-                                        "Meget gode",         
-                                        "Gode"            
-                                      )) 
+                      levels = c("Ved ikke",      
+                                 "Meget dårlige",              
+                                 "Dårlig",            
+                                 "Neutrale",            
+                                 "Meget gode",         
+                                 "Gode"            
+                      )) 
 
 ggplot(svar_df, aes(x = Svar, y = Antal)) +
   geom_bar(stat = "identity", fill = "blue") +
   geom_text(aes(label = round(Antal, 2)), position = position_dodge(width = 0.9), vjust = -0.5) + 
   theme_minimal() +
-  labs(title = "Flest folk synes der er Gode mulighed for at låne penge til sin virksomhed",
+  labs(title = "Flertallet synes der er gode muligheder for at låne penge til sin virksomhed",
        x = "Svarmulighed",
        y = "Antal svar") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(size = 10))
 
 # for uddybe opg.1 mht. Baums artikler
-# vi vil gerne se hvad har folk svaret ift. størrelse af virksomhed og det gør vi ved deler virksomhedens størrelse med antal ansatte og svarmuligheder med nettotal
+# vi vil gerne se hvad har folk svaret ift. størrelse af virksomhed og det gør vi ved at vi deler virksomhedens størrelse med antal af ansatte og svarmuligheder med nettotal
 df1.1 <- df %>%
   mutate(`Hvordan ser du mulighederne for at låne penge til din virksomhed? (fiktivt spørgsmål)` = case_when(
     `Hvordan ser du mulighederne for at låne penge til din virksomhed? (fiktivt spørgsmål)` == "Meget gode" ~ 1,
@@ -126,7 +126,7 @@ df_nettotal <- df1.1 %>%
   group_by(`Antal ansatte Cvr-nr.`) %>%
   summarise(nettotal = mean(`Hvordan ser du mulighederne for at låne penge til din virksomhed? (fiktivt spørgsmål)`, na.rm = TRUE))
 
-# omdanne til facotr med levels for specifik rækkefølge
+# omdanne til factor med levels for en specifik rækkefølge
 df_nettotal$`Antal ansatte Cvr-nr.` <- factor(df_nettotal$`Antal ansatte Cvr-nr.`, 
                                               levels = c("1-20 ansatte", 
                                                          "21-50 ansatte", 
@@ -144,10 +144,10 @@ ggplot(df_nettotal, aes(x = `Antal ansatte Cvr-nr.`, y = nettotal)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),plot.title = element_text(size = 12))
 
-# Nu vil vi gerne se med økonomisk perpektiv siden antallet af medarbejdere er ikke alene afgørende lånemulighederne
-# Vi ved virksomhed med højt indtjening på aktiver er nemmere til at låne penge. Og jo robuste virksomheder er jo bedre adgang til lån
-# Derfortil det næste graf kigge vi på afkastningsgrad og soliditetsgrad
-# Vi skal første have gennemsnit af afkastningsgrad og soliditetsgrad fordi kolonner er blev delt fra 2016-2021
+# Nu vil vi gerne se med et økonomisk perpektiv siden antallet af medarbejdere ikke alene er afgørende lånemulighederne
+# Vi ved virksomheder med høj indtjening på aktiver nemmere kan låne penge. Mere robuste virksomheder har bedre udsigter til lån
+# Vi kigger derfor på afkastningsgrad og soliditetsgrad i de næste grafer
+# Vi skal først have gennemsnit af afkastningsgrad og soliditetsgrad fordi kolonnerne er blevet delt fra 2016-2021
 
 df_virksomhed_gns <- df %>%
   rowwise() %>%
@@ -198,17 +198,17 @@ df_long <- df_svar_gns %>%
   )
 
 df_long$svar_kategorii = factor(df_long$svar_kategori, 
-                       levels = c( "Meget dårlige/Dårlige",
-                                   "Neutrale",
-                                   "Gode/Meget gode"
-                                   ))
+                                levels = c( "Meget dårlige/Dårlige",
+                                            "Neutrale",
+                                            "Gode/Meget gode"
+                                ))
 
 
 ggplot(df_long, aes(x = type, y = værdi, fill = svar_kategorii)) +
   geom_col(position = position_dodge(width = 0.8)) +
   geom_text(aes(label = round(værdi, 2)), position = position_dodge(width = 0.9),
             vjust = -0.5) +  
-  labs(title = "Virksomheder med et højere soliditetsgrad synes\nbedre om finansieringsklimaet",
+  labs(title = "Virksomheder med en højere soliditetsgrad synes\nbedre om finansieringsklimaet",
        y = "Pct.",
        x = "",
        fill = "") +
@@ -253,12 +253,12 @@ df_CLM <- df %>%
                       na.rm = TRUE),
     # Beregn gennemsnit balance per virksomhed
     gns_balance = mean(c(`Balance 2021 (1.000 kr)`,
-                        `Balance 2020 (1.000 kr)`,
-                        `Balance 2019 (1.000 kr)`,
-                        `Balance 2018 (1.000 kr)`,
-                        `Balance 2017 (1.000 kr)`,
-                        `Balance 2016 (1.000 kr)`),
-                      na.rm = TRUE)
+                         `Balance 2020 (1.000 kr)`,
+                         `Balance 2019 (1.000 kr)`,
+                         `Balance 2018 (1.000 kr)`,
+                         `Balance 2017 (1.000 kr)`,
+                         `Balance 2016 (1.000 kr)`),
+                       na.rm = TRUE)
   ) %>%
   ungroup
 
@@ -268,14 +268,14 @@ df_CLM$lånmuligheder <- factor(df_CLM$`Hvordan ser du mulighederne for at låne
                                labels = c("Meget dårlige", "Dårlig", "Neutrale", "Gode", "Meget gode"),
                                ordered = TRUE)
 
-df_CLM_clean <- df_CLM_clean %>%
+df_CLM_clean <- df_CLM %>%
   mutate(across(c(gns_soliditet, gns_afkast, gns_balance), 
                 ~scale(.) %>% as.vector))
 
 model_clm <- clm(lånmuligheder~ gns_soliditet + 
-                  gns_afkast + 
-                  gns_balance, 
-                data = df_CLM_clean, link = "logit")
+                   gns_afkast + 
+                   gns_balance, 
+                 data = df_CLM_clean, link = "logit")
 
 
 summary(model_clm)
@@ -329,7 +329,7 @@ df_3g <- df_3 %>%
 df_3g$Balance <- log(df_3g$Balance)
 
 # Omstrukturering til long format
-df_3long <- df_3g %>%
+df_3long <- df_3 %>%
   pivot_longer(
     cols = c(gns_soliditet, gns_afkast, gns_balance),
     names_to = "type",
@@ -338,15 +338,15 @@ df_3long <- df_3g %>%
 
 # Sæt korrekt rækkefølge på faktor levels
 df_3g$svar_kategori <- factor(df_3g$svar_kategori,
-                                 levels = c("Meget dårlige/Dårlige",
-                                            "Neutrale",
-                                            "Gode/Meget gode"))
+                              levels = c("Meget dårlige/Dårlige",
+                                         "Neutrale",
+                                         "Gode/Meget gode"))
 
 # Plot
 ggplot(df_3g, aes(x = svar_kategori, y = Balance, fill = svar_kategori)) +
   geom_col(position = position_dodge(width = 0.8)) +
   geom_text(aes(label = round(Balance, 2)), vjust = -0.5) +  
-  labs(title = "Virksomheder med et højere balance\nsynes bedre om finansieringsklimaet",
+  labs(title = "Virksomheder med en højere balance\nsynes bedre om finansieringsklimaet",
        y = "log(balance)",
        x = "",
        fill = "") +
@@ -359,4 +359,35 @@ ggplot(df_3g, aes(x = svar_kategori, y = Balance, fill = svar_kategori)) +
   ) +
   scale_y_continuous(limits = c(0, 14),
                      breaks = seq(0, 14, by = 2))
+
+
+
+
+
+
+
+
+
+# Tilføj etableringsdato som X variabel i modellen
+library(lubridate)
+
+df_CLM <- df_CLM %>%
+  mutate(
+    alder = year(Sys.Date()) - year(dmy(Etableringsdato))
+  )
+
+df_CLM_clean_alder <- df_CLM %>%
+  mutate(across(c(gns_soliditet, gns_afkast, gns_balance, alder), 
+                ~scale(.) %>% as.vector))
+
+alder_clm <- clm(lånmuligheder ~ gns_soliditet + 
+                   gns_afkast + 
+                   gns_balance + 
+                   alder, 
+                 data = df_CLM_clean_alder, 
+                 link = "logit")
+summary(alder_clm)
+
+
+
 
